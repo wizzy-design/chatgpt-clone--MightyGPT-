5d95bcf68ed5137d3ebee7d23751dd785be26e08
Wisdom Ochei <wisdomofgod2003@gmail.com>
2024-03-10 14:11:44 +0100

    Done with Mobile styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,11 +7,17 @@ const Sidebar = ({ open }) => {
     <section
       className={`${
         open ? "translate-x-0" : "translate-x-100 hidden"
-      } z-10 absolute flex-col justify-between bg-[#202123] h-screen w-[244px]`}
+      } z-10 absolute flex-col items-center justify-between bg-[#202123] h-screen w-[70%]`}
     >
-      <button className="m-2.5 p-2.5 bg-transparent rounded-[5px] border-solid border-[0.5px] border-[rgba(255_255_255_0.5)]">
-        <img src={icon} alt="ChatGPT logo" />
-        New chat
+      <button className="flex m-2.5 py-2 px-1.5 rounded-xl w-[92%] justify-between items-center hover:bg-[#242424] focus:bg-[#242424]">
+        <span className="flex items-center text-sm font-semibold gap-x-2">
+          <img
+            src={icon}
+            alt="ChatGPT logo"
+            className="w-[1.7rem] p-1 rounded-full bg-white "
+          />
+          New chat
+        </span>
         <span>
           <FaPenToSquare />
         </span>
